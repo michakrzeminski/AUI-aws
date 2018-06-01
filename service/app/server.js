f47bf5ad310749ce2a031f124620286f9d730c6f
michakrzeminski <micha.krzeminski@gmail.com>
2018-06-01 19:02:32 +0200

    cors update

--- service/app/server.js
+++ service/app/server.js
@@ -5,15 +5,21 @@ const db = require('./db.json');
 app.use(cors())
 
 app.get('/users', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.json({msg: db.users})
 });
 
 app.get('/users/:userId', function (req, res, next) {
   const id = parseInt(req.params.userId);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.json({msg: db.users.find((user) => user.id == id)})
 });
 
 app.get('/', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.json({msg: 'Ready to receive requests'});
 });
 
